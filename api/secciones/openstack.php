453029a68c0083e24ba315bd4e902837fc427886
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:33:19 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "OpenStack lanza su nueva versión: Yoga",
+    "autor" => "Juan Pérez",
+    "resumen" => "La comunidad de OpenStack ha anunciado el lanzamiento de su nueva versión, Yoga, que incluye mejoras significativas en la gestión de redes y el almacenamiento, así como nuevas características que facilitan la integración con plataformas de inteligencia artificial."
 ];
 ?>
