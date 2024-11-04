18a050ffc440a26aef2470ac237fd826af326ced
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:26:34 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El PIB crece un 3% en el último trimestre del año",
+    "autor" => "Carlos Ramírez",
+    "resumen" => "La última estimación del Producto Interno Bruto (PIB) muestra un crecimiento del 3% en el último trimestre, impulsado por un aumento en la inversión pública y la recuperación del consumo, lo que sugiere una tendencia positiva para la economía nacional."
 ];
 ?>
