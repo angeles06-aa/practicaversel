89be1492b318c0d6489dc346ca6f9526260d8e35
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:23:59 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren un nuevo exoplaneta que podría ser habitable",
+"autor" => "Investigadores del Observatorio Astronómico de la Universidad de Tokio",
+"resumen" => "Un equipo de astrónomos ha identificado un exoplaneta en la zona habitable de su estrella, donde las condiciones podrían permitir la existencia de agua líquida. Este hallazgo aumenta las posibilidades de encontrar vida fuera de nuestro sistema solar y proporciona nuevos datos para futuras investigaciones sobre la habitabilidad de exoplanetas.",
 ];
 ?>
