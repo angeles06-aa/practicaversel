0ed27e2e86dfe6316be598ae6d7931ff82f837b8
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:33:51 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La importancia de la educación digital en el siglo XXI",
+    "autor" => "Ana Torres",
+    "resumen" => "En un mundo cada vez más digitalizado, es crucial que las instituciones educativas adapten sus currículos para incluir competencias digitales, preparando a los estudiantes para un futuro laboral que exige habilidades tecnológicas avanzadas."
 ];
 ?>
