bd9a02d997427c0942a9bdc930c4a8f2bfa749cb
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:25:32 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La apertura de un nuevo museo dedicado al arte contemporáneo en la ciudad",
+"autor" => "María López",
+"resumen" => "Un nuevo museo ha abierto sus puertas en el corazón de la ciudad, dedicado a promover el arte contemporáneo. La colección incluye obras de artistas emergentes y reconocidos, ofreciendo un espacio de reflexión y diálogo sobre las tendencias actuales en el arte.",
 ];
 ?>
