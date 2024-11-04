637d96353fecb60017326430765e6e8e3c288211
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:29:48 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Git 2.39 se lanza con nuevas características y mejoras",
+    "autor" => "Fernando Torres",
+    "resumen" => "La última versión de Git, la 2.39, introduce mejoras significativas en el rendimiento y nuevas características como el soporte mejorado para las submódulos y cambios en la interfaz de usuario, facilitando la experiencia del desarrollador."
 ];
 ?>
