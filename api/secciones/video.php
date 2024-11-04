2c03d7f8d66178420a759722609d6098107c6b54
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:36:53 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "YouTube lanza nuevas funciones para creadores de contenido",
+    "autor" => "Sofía Hernández",
+    "resumen" => "La plataforma ha introducido herramientas innovadoras para ayudar a los creadores a aumentar su audiencia y monetizar su contenido, incluyendo un nuevo sistema de análisis y opciones de colaboración."
 ];
 ?>
