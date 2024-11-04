a163eddb70578f793e5245cee0675fa1eb812d64
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:30:22 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Cumbre Mundial sobre el Cambio Climático concluye con acuerdos históricos",
+    "autor" => "Sofía Martínez",
+    "resumen" => "La reciente cumbre mundial sobre el cambio climático ha concluido con acuerdos significativos entre naciones para reducir las emisiones de gases de efecto invernadero y fomentar la energía renovable, marcando un paso importante hacia un futuro sostenible."
 ];
 ?>
