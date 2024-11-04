ceac310922033d4d8f021d789da1a86ef0674075
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:28:03 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "Nuevos programas de formación en habilidades digitales",
+    "autor" => "sistemas@uns",
+    "resumen" => "Las universidades han lanzado nuevos programas de formación en habilidades digitales, incluyendo cursos de programación, ciberseguridad y análisis de datos, para preparar a los estudiantes para el mercado laboral actual."
 ];
 ?>
