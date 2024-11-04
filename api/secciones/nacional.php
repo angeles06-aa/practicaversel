5d70c93aefc6a2ca3b30b0c4a6acda45e41d31d8
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:31:21 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Gobierno lanza nuevo programa de apoyo a pequeñas y medianas empresas",
+    "autor" => "Juan Pérez",
+    "resumen" => "El gobierno ha anunciado un ambicioso programa destinado a apoyar a las pequeñas y medianas empresas (PYMEs) con financiamiento, capacitación y acceso a mercados, buscando estimular la economía local y fomentar la creación de empleo."
 ];
 ?>
