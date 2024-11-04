f2863fd006214b087b154c668369bc7c6720cd3d
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:28:53 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Reconocido actor recibe homenaje por su carrera",
+    "autor" => "María López",
+    "resumen" => "El famoso actor Juan Pérez fue homenajeado en una ceremonia especial por su destacada trayectoria en el cine y la televisión, donde se destacó su compromiso con el arte y su impacto en la industria."
 ];
 ?>
