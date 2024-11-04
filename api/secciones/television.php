52a1eefaccac340543b4790f776b6241db100b24
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:36:19 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El regreso de las series clásicas en formato reboot",
+    "autor" => "Laura Martínez",
+    "resumen" => "Las productoras están apostando por revivir series icónicas de los 90 y 2000, adaptándolas a los gustos actuales del público, lo que ha generado tanto entusiasmo como críticas por la falta de originalidad."
 ];
 ?>
