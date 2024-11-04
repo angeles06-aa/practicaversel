6176f0a3f4c5ccba424ccd6ca85d1f90b63c6b59
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:27:13 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Aumento del empleo en el sector tecnológico durante 2024",
+    "autor" => "María Fernanda",
+    "resumen" => "El sector tecnológico ha visto un aumento del 15% en la contratación de nuevos empleados durante el primer trimestre de 2024, impulsado por la demanda de talento en áreas como inteligencia artificial y desarrollo de software."
 ];
 ?>
