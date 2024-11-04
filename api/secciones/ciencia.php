e0035b995b9b156056d09580ea15224221b91df5
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:18:55 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "LA TEORIA DE LA RELATIVIDAD",
+"autor" => "Albert Einstein",
+"resumen" => "La teoría de la relatividad revolucionó la forma en que entendemos el espacio y el tiempo, introduciendo conceptos como la dilatación del tiempo y la contracción de la longitud en objetos en movimiento a altas velocidades.",
 ];
 ?>
