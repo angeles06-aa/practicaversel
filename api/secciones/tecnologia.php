1b2f9089fca4b8b521a46ad8a38533b27b0f22f3
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:35:49 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El auge de la inteligencia artificial en el sector empresarial",
+    "autor" => "María Gómez",
+    "resumen" => "Las empresas están adoptando rápidamente la inteligencia artificial para mejorar la eficiencia operativa y ofrecer experiencias personalizadas a sus clientes, pero también surgen preocupaciones sobre la ética y el empleo en el futuro."
 ];
 ?>
