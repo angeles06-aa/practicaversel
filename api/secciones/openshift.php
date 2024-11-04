815366791a48ade44ccbf56608bd2333a637a000
angeles06-aa <suyonangeles25@gmail.com>
2024-11-04 10:32:12 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -1,11 +1,13 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORÍA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Red Hat lanza la nueva versión de OpenShift 4.10",
+    "autor" => "Andrés López",
+    "resumen" => "Red Hat ha anunciado el lanzamiento de OpenShift 4.10, que incluye mejoras en la seguridad, soporte para nuevas arquitecturas y optimizaciones en la gestión de aplicaciones, lo que permitirá a los desarrolladores construir y desplegar aplicaciones más fácilmente en entornos híbridos y en la nube."
 ];
 ?>
+
+
